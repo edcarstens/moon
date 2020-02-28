74a3952720bc724ed6c5faa949fab10dc5aa35e8
Edward Carstens <Edward.Carstens@microchip.com>
2020-02-27 20:25:13 -0600

    big update of pet files

--- petsetup.sh
+++ petsetup.sh
@@ -1,6 +1,6 @@
 # Source this file to setup PET and make under Windows
 alias make='/c/Program\ Files/mingw-w64/i686-8.1.0-posix-dwarf-rt_v6-rev0/mingw32/bin/mingw32-make.exe'
-export PET_INSTALL_PATH=/c/pet
+export PET_INSTALL_PATH=/c/sg702cana0_scripts/pet
 
-# For debugging the app use local port
+# For debugging the app use local port (http://localport:4200)
 export PORT=4200
