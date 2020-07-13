9eb25f3199ab78c46aefd2b73d87ef2d38323687
Edward Carstens <Edward.Carstens@microchip.com>
2020-07-13 07:50:51 -0500

    add submodule fastjs, start converting pet files to fast files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,8 @@
-var socket = io();
-
-// Hide the address bar on iphone safari
-window.addEventListener("load", function() {
-    setTimeout(function() {
-	window.scrollTo(0,0);
-    }, 0);
-});
-
-let width = $(window).width();
-let height = $(window).height();
-$('#maintop').width(width);
-$('#maintop').height(height);
-$('#inputChatName').css("left", width/2 - 200);
-$('#inputChatName').css("top", height/2 - 50);
-$('#btnPlay').css("left", width/2 - 45);
-$('#btnPlay').css("top", height/2 - 50 + 70);
-console.log('Welcome to MOON')
-moon.init(
-    socket
-)
-moon.start()
+var socket = io();
+
+// Hide the address bar on iphone safari
+window.addEventListener("load", function() {
+    setTimeout(function() {
+	window.scrollTo(0,0);
+    }, 0);
+});
