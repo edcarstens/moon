b4b80c14b4fa2296a3ea7eebe605eb151a7afc6f
Ed Carstens <edcarstens@yahoo.com>
2020-07-25 16:49:22 -0500

    fix a few errors, app runs locally

--- gameclerkClass.js
+++ gameclerkClass.js
@@ -1,10 +1,10 @@
-let clerkClass = require('./clerkClass')
-let gameroomClass = require('./gameroomClass')
-class gameclerkClass extends clerkClass {
-    newRoom(
-        roomId
-    ) {
-        return new gameroomClass(this.io, roomId, this.clientsPerRoom)
-    }
-}
-module.exports = gameclerkClass
+let clerkClass = require('./clerkClass')
+let gameroomClass = require('./gameroomClass')
+class gameclerkClass extends clerkClass {
+    newRoom(
+        roomId,
+    ) {
+        return new gameroomClass(this.io, roomId, this.clientsPerRoom)
+    }
+}
+module.exports = gameclerkClass
--- playerClass.js
+++ playerClass.js
@@ -1,15 +1,15 @@
-class playerClass {
-    constructor(
-        socket=null,
-        nickName=""
-    ) {
-        this.socket = socket
-        this.nickName = nickName
-        this.bid = 0
-        this.tricks = 0
-        this.score = 0
-        this.wins = 0
-        this.idx = 0
-    }
-}
-module.exports = playerClass
+class playerClass {
+    constructor(
+        socket=null,
+        nickName="",
+    ) {
+        this.socket = socket
+        this.nickName = nickName
+        this.bid = 0
+        this.tricks = 0
+        this.score = 0
+        this.wins = 0
+        this.idx = 0
+    }
+}
+module.exports = playerClass
