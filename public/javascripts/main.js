63b2c5b81a7bc30815652e6197bbaa31caf251ca
Edward Carstens <Edward.Carstens@microchip.com>
2020-07-25 09:52:29 -0500

    remove fast target files from git repo

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-var socket = io();
-
-// Hide the address bar on iphone safari
-window.addEventListener("load", function() {
-    setTimeout(function() {
-	window.scrollTo(0,0);
-    }, 0);
-});
